85aea56fcee69caf170a8ea6210f65cf743190cc
Manish <maniquare@gmail.com>
2026-03-16 05:46:18 +0000

    bsf10

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+export default function Footer() {
+  return (
+    <footer className="flex justify-between p-4 text-sm border-t bg-white">
+      <div>
+        <b>bsf1010pro</b> | Powered by <b>bsf1010pro Gaming</b>
+      </div>
+
+      <div>
+        Agent Panel v2.0.0
+      </div>
+    </footer>
+  )
+}
